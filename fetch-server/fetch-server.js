df90450da8fb2414fb9e06b89dbfbb1f4627fad7
SerhanOrhan <79594171+SerhanOrhan@users.noreply.github.com>
2022-04-04 11:54:56 +0300

    Dtp ve Ph  filterelemeleri hazır.

--- fetch-server/fetch-server.js
+++ fetch-server/fetch-server.js
@@ -4,16 +4,12 @@ const reportController = {
     index: (req, res) => {
         return axios.get('https://seffaflik.epias.com.tr/transparency/service/market/intra-day-trade-history', { params: req.query })
             .then(response => {
-                const manipulatedData=response.data.body.intraDayTradeHistoryList;
-                    console.log(manipulatedData.conract)
-                    // manipulatedData & manipulatedData.map((item,index)=>{
-                    //     const regexKod=/[0-9]/gm;
-                    // if (item.conract.str.replace(regexKod,"")=="PH") {
-                    //     console.log(item.conract);
-                    // }
-                    //     return item;
-                    // })
-                return res.send(manipulatedData)
+                const manipulatedData = response.data.body.intraDayTradeHistoryList;
+                let maps = manipulatedData && manipulatedData.filter((item) => {
+                   return item.conract.substring(0, 2) == "PH";
+                })
+
+                return res.send(maps);
             })
             .catch(error => res.send(error))
     }
--- react-front-end/src/Home.js
+++ react-front-end/src/Home.js
@@ -1,30 +1,42 @@
 import React from 'react'
 import axios from 'axios'
-import { Table, DatePicker, Space,Label } from 'antd'
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import locale from 'antd/lib/locale/zh_CN';
+import { Table, DatePicker, Space } from 'antd'
 import { useEffect } from 'react'
 import { useState } from 'react'
 
 function Home() {
     const [data, setData] = useState([]);
+    const [startDate, setStartDate] = useState("2022-01-26");
+    const [endDate, setEndDate] = useState("2022-01-26");
+    const [btn, setBtn] = useState(null);
+
+
+
+
+
 
     const getReport = () => {
-        axios.get('http://localhost:8090/transparency/service/market/intra-day-trade-history?endDate=2022-01-26&startDate=2022-01-26')
+        const endDatee = `?endDate=${endDate}&startDate=${startDate}`;
+        console.log(endDatee)
+        axios.get('http://localhost:8090/transparency/service/market/intra-day-trade-history' + endDatee)
             .then(response => setData(response))
     }
 
     useEffect(() => {
-       console.log(data);
+        console.log(data);
     }, [data])
 
     useEffect(() => {
         getReport();
-    }, [])
-
+    }, [btn])
 
     let columns = [
         {
             title: 'Conract',
-            dataIndex:'conract'
+            dataIndex: 'conract'
         },
         {
             title: 'ID',
@@ -40,21 +52,34 @@ function Home() {
         },
         {
             title: 'Tarih',
-            dataIndex:'date'
+            dataIndex: 'date'
         }
     ]
-    function onChange(date, dateString) {
-        console.log(date, dateString);
-      }
+    function onChangeStart(date, dateString) {
+        setStartDate(dateString);
+       // console.log(startDate);
+    }
+    function onChangeEnd(date, dateString) {
+        setEndDate(dateString);
+        //console.log(endDate);
+    }
+
 
     return (
         <div>
-            <Space direction="horzintal">
-                <div>Başlangıc Tarihi</div>
-                <DatePicker onChange={onChange}/>
-                <div>Bitiş Tarihi</div>
-                <DatePicker onChange={onChange}/>
-            </Space>,
+            <Space direction="horzintal" locale={locale}>
+                <div>
+                    <div>Başlangıc  Tarihi</div>
+                    <DatePicker onChange={onChangeStart} defaultValue={moment('2015-01-01', 'YYYY-MM-DD')} />
+                </div>
+                <div>
+                    <div>Bitiş Tarihi</div>
+                    <DatePicker onChange={onChangeEnd} defaultValue={moment('2015-01-02', 'YYYY-MM-DD')} />
+                </div>
+                <div>
+                    <button onClick={() => { setBtn(btn + 1) }}>Listele</button>
+                </div>
+            </Space>
             <Table dataSource={data.data} columns={columns} />
         </div>
     )
